04e5eb5e5776c8d696cbeb5a1f374baa0ef17a3f
theWaryuk <66172972+theWaryuk@users.noreply.github.com>
2023-02-12 23:07:32 +0200

    Menu button working

--- Assets/Scripts/UITitleScreen.cs
+++ Assets/Scripts/UITitleScreen.cs
@@ -1,7 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class UITitleScreen : MonoBehaviour
 {
+    public void LoadMainScene()
+    {
+        SceneManager.LoadScene(1);
+    }
 }
